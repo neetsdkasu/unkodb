504bdcd57038b8f7b2e88855f590fdd8bad25f09
Leonardone <neetsdkasu@gmail.com>
2022-05-31 04:22:58 +0900

    うーむ・・・

--- file.go
+++ file.go
@@ -2,7 +2,7 @@
 // author: Leonardone @ NEETSDKASU
 
 //
-// ファイルフォーマット
+// ファイルヘッダフォーマット
 //  シグネチャ
 ///   16 byte
 //      3 5 7 11 13 17 19 23 29 31 'U' 'N' 'K' 'O' 'D' 'B'
@@ -26,24 +26,26 @@ import (
 var byteOrder = binary.BigEndian
 
 const (
+	AddressSize = 4
+
 	FileHeaderSignaturePosition = 0
 	FileHeaderSignatureLength   = 16
 
 	FileHeaderFileFormatVersionPosition = FileHeaderSignaturePosition + FileHeaderSignatureLength
-	FileHeaderFileFormatVersionLength   = 4
+	FileHeaderFileFormatVersionLength   = 2
 
 	FileHeaderPreserveAreaPosition = FileHeaderFileFormatVersionPosition + FileHeaderFileFormatVersionLength
-	FileHeaderPreserveAreaLength   = 4
+	FileHeaderPreserveAreaLength   = AddressSize
 
 	FileHeaderTableListRootAddressPosition = FileHeaderPreserveAreaPosition + FileHeaderPreserveAreaLength
-	FileHeaderTableListRootAddressLength   = 4
+	FileHeaderTableListRootAddressLength   = AddressSize
 
-	FileHeaderIdleSegmentListRootAddressPosition = FileHeaderTableListRootAddressPosition +
-		FileHeaderTableListRootAddressLength
-	FileHeaderIdleSegmentListRootAddressLength = 4
+	FileHeaderIdleSegmentListRootAddressPosition = FileHeaderTableListRootAddressPosition + FileHeaderTableListRootAddressLength
+	FileHeaderIdleSegmentListRootAddressLength   = AddressSize
 
-	FileHeaderSize = FileHeaderIdleSegmentListRootAddressPosition +
-		FileHeaderIdleSegmentListRootAddressLength
+	FileHeaderSize = FileHeaderIdleSegmentListRootAddressPosition + FileHeaderIdleSegmentListRootAddressLength
+
+	SegmentHeaderSize = AddressSize
 )
 
 type File struct {
@@ -90,10 +92,9 @@ func (file *File) checkHeader() error {
 }
 
 func (file *File) checkSignature() error {
-	buffer, err := file.Read(FileHeaderSignaturePosition,
-		FileHeaderSignatureLength)
+	buffer, err := file.Read(FileHeaderSignaturePosition, FileHeaderSignatureLength)
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed File.checkSignature [%w]", err)
 	}
 	if !bytes.Equal(buffer, Signature()) {
 		return fmt.Errorf("Wrong Signature in File Header")
@@ -101,30 +102,80 @@ func (file *File) checkSignature() error {
 	return nil
 }
 
-func (file *File) Read(position, length int) ([]byte, error) {
+func (file *File) ReadToBuffer(position int, buffer []byte) error {
 	if _, err := file.inner.Seek(int64(position), io.SeekStart); err != nil {
-		return nil, err
+		return err
 	}
+	if n, err := io.ReadFull(file.inner, buffer); err != nil {
+		return err
+	} else if n != len(buffer) {
+		return fmt.Errorf("Cannot Read (Position: %d, Length: %d, Read: %d)", position, len(buffer), n)
+	}
+	return nil
+}
+
+func (file *File) Read(position, length int) ([]byte, error) {
 	buffer := make([]byte, length)
-	if n, err := file.inner.Read(buffer); err != nil {
+	err := file.ReadToBuffer(position, buffer)
+	if err != nil {
 		return nil, err
-	} else if n != length {
-		return nil, fmt.Errorf("Cannot Read (Position: %d, Length: %d)", position, length)
 	}
 	return buffer, nil
 }
 
-func (file *File) ReadTableListRootAddress() (int, error) {
-	buffer, err := file.Read(
-		FileHeaderTableListRootAddressPosition,
-		FileHeaderTableListRootAddressLength)
-	if err != nil {
-		return 0, err
+func (file *File) Write(position int, data []byte) error {
+	if _, err := file.inner.Seek(int64(position), io.SeekStart); err != nil {
+		return err
 	}
-	address := byteOrder.Uint32(buffer)
-	return int(address), nil
+	if n, err := file.inner.Write(data); err != nil {
+		return err
+	} else if n != len(data) {
+		return fmt.Errorf("Cannot Write (Position: %d, Data Length: %d, Wrote: %d)", position, len(data), n)
+	}
+	return nil
+}
+
+func (file *File) ReadSegment(position int) (*Segment, error) {
+	var headerBuffer [SegmentHeaderSize]byte
+	err := file.ReadToBuffer(position, headerBuffer[:])
+	if err != nil {
+		return nil, fmt.Errorf("Failed File.ReadSegment (read size) [%w]", err)
+	}
+	size := int(byteOrder.Uint32(headerBuffer[:]))
+	buffer, err := file.Read(position, size)
+	if err != nil {
+		return nil, fmt.Errorf("Failed File.ReadSegment (read data) [%w]", err)
+	}
+	seg := &Segment{file, position, buffer}
+	return seg, nil
+}
+
+func (file *File) ReadTableListRootAddress() (int, error) {
+	var buffer [FileHeaderTableListRootAddressLength]byte
+	err := file.ReadToBuffer(FileHeaderTableListRootAddressPosition, buffer[:])
+	if err != nil {
+		return 0, fmt.Errorf("Failed File.ReadTableListRootAddress [%w]", err)
+	}
+	address := int(byteOrder.Uint32(buffer[:]))
+	return address, nil
+}
+
+func (file *File) WriteTableListRootAddress(newAddress int) error {
+	panic("TODO")
+}
+
+func (seg *Segment) Position() int {
+	return seg.position
 }
 
 func (seg *Segment) Buffer() []byte {
-	return seg.buffer
+	return seg.buffer[SegmentHeaderSize:]
+}
+
+func (seg *Segment) Flush() error {
+	err := seg.file.Write(seg.position, seg.buffer)
+	if err != nil {
+		return fmt.Errorf("Failed Segment.Flush [%w]", err)
+	}
+	return nil
 }
